4e383fbf193f62100bdb93518f4c52981ef5ece5
Starostin SV <Vlillen@yandex.ru>
2023-02-10 00:38:45 +0400

    add Example Hello User

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("_Hello, World!_");
---
+++ Example001_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ваше имя "); // Вывод текста в терминал с новой строки
+string username = Console.ReadLine();
+Console.Write("Привет, "); // Вывод текста в терминал
+Console.Write(username); // Вывод результат из переменной username в терминал
