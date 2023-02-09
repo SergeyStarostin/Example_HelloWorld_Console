3aed6dfaf182fdb9f0fbe0df2d962e7359833825
Starostin SV <Vlillen@yandex.ru>
2023-02-10 01:20:13 +0400

    add Example Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 3; // Задаем переменную и присваиваем ей целое число 3
+int numberB = 5; // Задаем переменную и присваиваем ей целое число 5
+int result = numberA + numberB;
+Console.WriteLine (result); // Вывод в консоль сумму
