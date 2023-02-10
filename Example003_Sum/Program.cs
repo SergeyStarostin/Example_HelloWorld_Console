5dba78465fb1b793dc8a600a22382913aeb6767f
Y127908 <Sergey.starostin@vaz.ru>
2023-02-10 10:08:25 +0400

    Changed file name Example003_Sum.csproj

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,4 @@
 ﻿int numberA = new Random().Next(1, 10); // Задаем случайное целое число от 1 до 10
 int numberB = new Random().Next(1, 10); // Задаем случайное целое число от 1 до 10
 int result = numberA + numberB;
-Console.WriteLine (result); // Вывод в консоль сумму
+Console.WriteLine (result); // Вывод в консоль сумму 
