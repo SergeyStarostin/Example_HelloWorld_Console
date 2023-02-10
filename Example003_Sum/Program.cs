0b1d7f119e5542805ee9c8d203f8efe0c76a3ff2
Y127908 <Sergey.starostin@vaz.ru>
2023-02-10 15:56:51 +0400

    added random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
 ﻿int numberA = new Random().Next(1, 10); // Задаем случайное целое число от 1 до 10
+Console.Write("Случайное число А = "); 
+Console.WriteLine(numberA); // Выводим на экран это рандомное число А
 int numberB = new Random().Next(1, 10); // Задаем случайное целое число от 1 до 10
+Console.Write("Случайное число B = "); 
+Console.WriteLine(numberB); // Выводим на экран это рандомное число B
 int result = numberA + numberB;
-Console.WriteLine (result); // Вывод в консоль сумму 
+Console.Write ("Сумма чисел = ");
+Console.WriteLine (result); // Вывод в консоль сумму
