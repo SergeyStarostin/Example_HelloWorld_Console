e582edf03f7701d360bca5d504cb977f2b4cbfb2
Starostin SV <Vlillen@yandex.ru>
2023-02-15 21:06:32 +0400

    Нахождение max с помощью массива

---
+++ Еxample009_IntroArray3/Program.cs
@@ -0,0 +1,17 @@
+﻿void FillArray (int[] collection)
+{
+    int lenght = collection.Lenght;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection [index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count
+}
+
+int[] array = new int [10]; // Создали новый массив, в котором будет 10 элементов (по умолчанию он наполнен нулями)
