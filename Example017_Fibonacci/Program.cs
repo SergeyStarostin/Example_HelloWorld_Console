fd5efbc00bd233762d476b8e8baea5f969c70d3b
Starostin SV <Vlillen@yandex.ru>
2023-03-01 23:35:16 +0400

    Вычисление числа Фибоначчи

---
+++ Example017_Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вычисление числа Фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int InputInt(string Number) // Метод ввода массива
+{
+    Console.WriteLine(Number);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Fibonacci (int Number) // Метод вычисления чисел Фибоначчи
+{
+    if(Number == 1 || Number == 2) return 1;
+    else return (Fibonacci (Number - 1)) + (Fibonacci (Number - 2));
+}
+
+int Number = InputInt($"Введите число n: "); // Ввод числа n 
+Console.WriteLine($"Ряд чисел Фибоначчи: ");
+
+for (int i = 1; i < Number; i++)
+{
+    Console.WriteLine(Fibonacci(i)); // Вывод ряда чисел Фибоначчи
+}
