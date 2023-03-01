6a0ec07f1e2e08d09d8e7b6052561f46d8864986
Starostin SV <Vlillen@yandex.ru>
2023-03-01 22:14:45 +0400

    Вычисление факториала числа N с помощью рекурсии

---
+++ Example016_Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вычисление факториала числа N с помощью рекурсии.
+
+int Number = InputInt("Введите число N: ");
+
+int InputInt(string Number) // Метод ввода массива
+{
+    Console.WriteLine(Number);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double Factorial (int N)
+{
+    if(N == 1) return 1;
+    else return N * Factorial(N - 1);
+}
+
+for (int i = 1; i < Number; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
