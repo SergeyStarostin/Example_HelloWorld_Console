65cae02b80e743a5247701a430d41f46e21f4103
Starostin SV <Vlillen@yandex.ru>
2023-03-12 17:36:36 +0400

    Программа просмотра информации о файлах каталога

---
+++ Example023_DirInfo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа просмотра информации о файлах каталога
+
+void CatalogInfo(string path, string indent = "")
+// метод CatalogInfo, path - первый аргумент, путь к папке; indent - 2й аргумент визуализации структуры каталога
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path); // получаем информацию о каталоге, в который зашли
+    foreach (var currentCatalog in catalogs.GetDirectories()) // получаем массив всех файлов в этого каталога
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}"); // выводим информацию о текущей каталоге
+        CatalogInfo(currentCatalog.FullName, indent + " "); // рекурсивно просматриваем все каталоги
+    }
+    foreach (var item in catalogs.GetFiles()) // просматриваем все файлы каталогов
+    {
+        Console.WriteLine($"{indent}{item.Name}"); // выводим на экран всефайлы каталогов
+    }
+}
+string path = "D:/GB/Examples/Example023_DirInfo"; // переменной path присваиваем путь к каталогу
+CatalogInfo(path); // вызов метода CatalogInfo
