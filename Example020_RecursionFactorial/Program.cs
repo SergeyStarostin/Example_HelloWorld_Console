d8dcf52d2a63dbf07cd53ed396bf7cc6c96df721
Starostin SV <Vlillen@yandex.ru>
2023-03-12 15:41:53 +0400

    Факториал числа N

---
+++ Example020_RecursionFactorial/Program.cs
@@ -0,0 +1,17 @@
+﻿// Факториал числа N
+
+int FactorialFor(int n)
+{
+ int result = 1;
+ for (int i = 1; i <= n; i++) result *= i;
+ return result;
+}
+
+System.Console.WriteLine(FactorialFor(5));
+
+int FactorialRec(int n)
+{
+ if (n == 0) return 1;
+ else return n * FactorialRec(n - 1);
+}
+System.Console.WriteLine(FactorialRec(5));
